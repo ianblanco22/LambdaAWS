2e4c74479293de6685a5f1e3947c6e0c9eee1381
Camilo <camilogomezr25@gmail.com>
2025-04-10 14:19:56 -0500

    corregir nombres y poner parametros de Dynamo

--- src/main/java/co/com/semillero/model/entity/ClientEntity.java
+++ src/main/java/co/com/semillero/model/entity/ClientEntity.java
@@ -1,11 +1,13 @@
 package co.com.semillero.model.entity;
 
 import com.amazonaws.services.dynamodbv2.datamodeling.DynamoDBAttribute;
+import com.amazonaws.services.dynamodbv2.datamodeling.DynamoDBDocument;
 import lombok.Getter;
 import lombok.Setter;
 
 @Getter
 @Setter
+@DynamoDBDocument
 public class ClientEntity {
 
     @DynamoDBAttribute(attributeName = "strIdLlave")
--- src/main/java/co/com/semillero/service/DynamoService.java
+++ src/main/java/co/com/semillero/service/DynamoService.java
@@ -1,6 +1,5 @@
 package co.com.semillero.service;
 
-import co.com.semillero.model.Entity.ClientEntity;
 
 public class DynamoService {
 
