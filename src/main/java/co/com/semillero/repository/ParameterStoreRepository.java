72197647002d9d674e2706ef712f785252de9522
Camilo <camilogomezr25@gmail.com>
2025-04-11 11:29:37 -0500

    Crear los enums para los parametros store

--- src/main/java/co/com/semillero/repository/ParameterStoreRepository.java
+++ src/main/java/co/com/semillero/repository/ParameterStoreRepository.java
@@ -1,5 +1,6 @@
 package co.com.semillero.repository;
 
+import co.com.semillero.constants.ParameterKey;
 import co.com.semillero.model.ParameterStoreDTO;
 import co.com.semillero.util.ParameterStoreUtil;
 
@@ -7,15 +8,15 @@ import java.util.Map;
 
 public class ParameterStoreRepository {
     public ParameterStoreDTO getParameter(){
-        String path="/Semillero/capacitacion-semillero/";
+        
         ParameterStoreDTO parameter=new ParameterStoreDTO();
 
-        Map<String, String> parameterSemillero= ParameterStoreUtil.getParameters(path);
+        Map<String, String> parameterSemillero = ParameterStoreUtil.getParameters(ParameterKey.BASE_PATH);
 
-        parameter.setTable(parameterSemillero.get(path+"nombreTabla"));
-        parameter.setRegion(parameterSemillero.get(path+"region"));
-        parameter.setArnSecre(parameterSemillero.get(path+"arnSecret"));
-        parameter.setUrlDynamo(parameterSemillero.get(path+"dynamoEndpoint"));
+        parameter.setTable(parameterSemillero.get(ParameterKey.NOMBRE_TABLA.getFullKey()));
+        parameter.setRegion(parameterSemillero.get(ParameterKey.REGION.getFullKey()));
+        parameter.setArnSecre(parameterSemillero.get(ParameterKey.ARN_SECRET.getFullKey()));
+        parameter.setUrlDynamo(parameterSemillero.get(ParameterKey.URL_DYNAMO.getFullKey()));
 
         return parameter;
     }
