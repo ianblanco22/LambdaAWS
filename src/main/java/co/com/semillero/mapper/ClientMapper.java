8a484c9f53e295d2b804f863e062bac9d4761e5f
ianblanco22 <ianblanco@cbit-online.com>
2025-04-11 10:16:27 -0500

    Se ajusta el mapper

--- src/main/java/co/com/semillero/mapper/ClientMapper.java
+++ src/main/java/co/com/semillero/mapper/ClientMapper.java
@@ -1,12 +1,16 @@
 package co.com.semillero.mapper;
 
 import co.com.semillero.model.Client;
+import co.com.semillero.model.entity.AccountEntity;
 import co.com.semillero.model.entity.ClientEntity;
+import co.com.semillero.model.entity.KeyEntity;
 
 public class ClientMapper {
 
     public ClientMapper(Client client){
         ClientEntity clientEntity = new ClientEntity();
+        KeyEntity keyEntity = new KeyEntity();
+        AccountEntity accountEntity = new AccountEntity();
 
         String id = clientEntity.getStrIdLlave().getStrIdLlave();
         clientEntity.setId(id);
@@ -14,6 +18,18 @@ public class ClientMapper {
         clientEntity.setStrFirstName(client.getStrFirstName());
         clientEntity.setStrLastName(client.getStrLastName());
         clientEntity.setStrEmail(client.getStrEmail());
+        clientEntity.setStrAddress(client.getStrAddress());
+        clientEntity.setStrIdentificationType(client.getStrIdentificationType());
+        clientEntity.setStrIdentificationNum(client.getStrIdentificationNum());
+
+        keyEntity.setStrIdLlave();
+        keyEntity.setStrIdLlave();
+        clientEntity.
+
+        accountEntity.setStrAccount(client.getStrAccount());
+        accountEntity.
+
+
 
     }
 }
